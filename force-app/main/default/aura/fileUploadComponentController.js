93b6ed3f5ff89941b4f7e12078812a5610f1f49a
alonzoedwards184 <alonzoedwards184@gmail.com>
2024-06-28 20:56:40 -0500

    LWC Update

---
+++ force-app/main/default/aura/fileUploadComponentController.js
@@ -0,0 +1,8 @@
+({
+    handleFileUpload: function(component, event, helper) {
+        var uploadedFiles = event.getParam("files");
+        var fileName = uploadedFiles[0].name;
+        component.set("v.fileName", fileName);
+        helper.processFileUpload(component, fileName);
+    }
+})
---
+++ force-app/main/default/aura/fileUploadComponentHelper.js
@@ -0,0 +1,6 @@
+({
+    processFileUpload: function(component, fileName) {
+        // Handle file processing logic here
+        console.log("File uploaded: " + fileName);
+    }
+})
